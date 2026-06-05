2cc9cfd74f02fe658e30c46d9ce10b1240c5ce18
Ankita Kevale <kevaleankita@gmail.com>
2026-06-05 15:04:10 +0530

    updated runner class

--- src/test/java/com/tutorialninja/runners/TestRunner.java
+++ src/test/java/com/tutorialninja/runners/TestRunner.java
@@ -4,12 +4,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/features",
-        glue = "com.opencart.stepdefinitions",
-        plugin = {
-                "pretty",
-                "html:target/cucumber-report.html"
-        },
+	    features = "src/test/resources/features",
+	    glue = "com.tutorialninja.stepdefinitions", // Update this line!
+	    plugin = {"pretty", "html:target/cucumber-reports.html"},
         monochrome = true
 )
 
